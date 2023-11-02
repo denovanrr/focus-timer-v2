e33e839418d85a1a693926f9f8ec7bdee07c47d8
denovanrr <137625696+denovanrr@users.noreply.github.com>
2023-11-02 11:38:34 -0300

    musica-alterada

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -1,4 +1,5 @@
 export const buttonPressAudio = new Audio('./assets/button-press.wav')
 export const kitchenTimer = new Audio('./assets/kichen-timer.mp3')
-export const bgAudio = new Audio('./assets/bg-audio.mp3')
+// export const bgAudio = new Audio('./assets/bg-audio.mp3')
+export const bgAudio = new Audio('http://www.dominiopublico.gov.br/download/som/on000010.mp3')
 
